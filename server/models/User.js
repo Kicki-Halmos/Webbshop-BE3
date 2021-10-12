f96c3bdf112da44a1feebd72b4afc059ff1e85e3
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-10-12 15:00:54 +0200

    :sparkles: add user model

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
