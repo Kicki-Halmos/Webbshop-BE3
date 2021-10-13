b43961d7fd65f82515c68163f03377ab4f8508ef
Kicki Halmos <kickihalmos@gmail.com>
2021-10-13 21:11:46 +0200

    :sparkles: setup api functions

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: "http://localhost:3000",
-  });
+});
 
- const getProducts = () => api.get('/api/products');
- const createProduct = (title, description, price, brand,category, img) => api.post('/api/products', {title, description, price, brand, category, img});
- const updateProduct = (id, title, description, price, brand,category, img) => api.put(`/api/products/${id}`, {title, description, price, brand,category, img})
- const getProductItem = (id) => api.get(`/api/products/${id}`);
- const deleteProductItem = (id) => api.delete(`/api/products/${id}`);
+const getProducts = () => api.get('/api/products');
+const createProduct = (title,description,price,brand,category,img) => api.post('/api/products',{ title,description,price,brand,category,img });
+const updateProduct = (id,title,description,price,brand,category,img) => api.put(`/api/products/${id}`,{ title,description,price,brand,category,img })
+const getProductItem = (id) => api.get(`/api/products/${id}`);
+const deleteProductItem = (id) => api.delete(`/api/products/${id}`);
 
- export const productApis = {getProducts, createProduct, updateProduct, getProductItem, deleteProductItem};
+export const productApis = { getProducts,createProduct,updateProduct,getProductItem,deleteProductItem };
 
-  export default api;
+export default api;
