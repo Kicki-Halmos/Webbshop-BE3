14915a8a592a477ac86ed21e70e7fe861e56f336
Abdullahi Aden <Abbe6152@gmail.com>
2021-10-15 13:25:26 +0200

    ♻️bit of try & catch

--- server/controller/orderController.js
+++ server/controller/orderController.js
@@ -6,53 +6,66 @@ exports.getAllOrders = async (req, res) => {
 
   res.status(200);
   res.json({ data: allOrders });
-  console.log(allOrders);
 };
 
 // get one order
 exports.getSingleOrder = async (req, res) => {
-  const { id } = req.params;
-
-  const singleOrder = await Order.findOne({ id });
-
-  res.status(200);
-  res.json({ data: singleOrder });
-  console.log(singleOrder);
+  try {
+    const { id } = req.params;
+    const singleOrder = await Order.findOne({ id });
+    res.status(200);
+    res.json({ data: singleOrder });
+  } catch (error) {
+    return error;
+  }
 };
 
 // add new order
 exports.addNewOrder = async (req, res) => {
-  const { product, sent } = req.body;
+  try {
+    const {
+      product, sent, totalCost, deliveryCost,
+    } = req.body;
 
-  const order = new Order({
-    product,
-    sent,
-  });
+    const order = new Order({
+      product,
+      sent,
+      totalCost,
+      deliveryCost,
+    });
 
-  await order.save();
-  res.status(201);
-  res.json({ data: order });
-  console.log(order);
+    await order.save();
+    res.status(201);
+    res.json({ data: order });
+  } catch (error) {
+    return error;
+  }
 };
 // update order
 exports.uppdateOrder = async (req, res) => {
-  const { id } = req.params;
-  const { product, sent } = req.body;
+  try {
+    const { id } = req.params;
+    const { product, sent } = req.body;
 
-  const updatedOrder = await Order.findByIdAndUpdate(id, { product, sent },
-    { new: true });
+    const updatedOrder = await Order.findByIdAndUpdate(id, { product, sent },
+      { new: true });
 
-  res.status(200);
-  res.json({ data: updatedOrder });
-  console.log(updatedOrder);
+    res.status(200);
+    res.json({ data: updatedOrder });
+  } catch (error) {
+    return error;
+  }
 };
 // delete order
 exports.deleteOrder = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const deleteddOrder = await Order.findByIdAndDelete(id);
+    const deleteddOrder = await Order.findByIdAndDelete(id);
 
-  res.status(204);
-  res.json({ data: deleteddOrder });
-  console.log(deleteddOrder);
+    res.status(204);
+    res.json({ data: deleteddOrder });
+  } catch (error) {
+    return error;
+  }
 };
