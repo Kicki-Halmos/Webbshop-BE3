39a6830ef648020ec8f37fdb806f6f4b37ff6d48
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-10-15 16:00:39 +0200

    :art: change user context

---
+++ client/src/contexts/user-context.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserContext = React.createContext({
+    user: {},
+    getUser: () => {},
+    loginUser: () => {},
+    registerUser: () => {},
+    logoutUser: () => {}
+});
+
+export default UserContext;
