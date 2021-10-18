002b584055654875fd8ad5ed899ba2341af97639
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-10-18 17:55:05 +0200

    :sparkles: add updateUser to context

--- client/src/contexts/user-context.js
+++ client/src/contexts/user-context.js
@@ -3,6 +3,7 @@ import React from 'react';
 const UserContext = React.createContext({
     user: {},
     getUser: () => {},
+    updateUser: () => {},
     loginUser: () => {},
     registerUser: () => {},
     logoutUser: () => {}
