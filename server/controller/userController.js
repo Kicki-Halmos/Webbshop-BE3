97bd8cfd01b5a48053fd6076d5077279a29e052c
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-10-19 10:29:37 +0200

    :construction: work on update in usercontroller

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -10,11 +10,11 @@ exports.getUser = wrapAsync(async (req, res) => {
 });
 
 exports.update = wrapAsync(async (req, res) => {
-  const {
-    fullName, email, password, phoneNumber, address,
-  } = req.body;
-  const user = await User.findOneAndUpdate(req.params.id, {
-    fullName, email, password, phoneNumber, address,
+  const user = await User.findOneAndUpdate({ _id: req.body.id }, {
+    fullName: req.body.fullName,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    address: req.body.address,
   }, { new: true });
   return res.status(200).json({ data: user });
 });
