a2d4b86df7a9bcc33fc14ac0aadea3d16d20a03e
Kicki Halmos <kickihalmos@gmail.com>
2021-10-20 22:45:13 +0200

    :bug:fix updateCart function in controller

--- server/controller/cartController.js
+++ server/controller/cartController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable eqeqeq */
 /* eslint-disable no-undef */
 /* eslint-disable consistent-return */
@@ -18,7 +19,7 @@ exports.getCart = wrapAsync(async (req, res) => {
 
 exports.addNewCart = wrapAsync(async (req, res) => {
   const { product, quantity } = req.body;
-  console.log(req.body);
+
   await new Cart({ products: [{ product, quantity }], userId: req.user._id })
     .save();
   const cart = await Cart.findOne({ userId: req.user._id })
@@ -27,31 +28,27 @@ exports.addNewCart = wrapAsync(async (req, res) => {
 });
 
 exports.updateCart = wrapAsync(async (req, res) => {
-  const { product, quantity } = req.body;
   let updatedProducts = [];
   const cart = await Cart
-    .findOne({ userId: req.user._id })
-    .populate('products.product');
-  const existingProduct = cart.products.map((item) => {
-    if (item.product._id == product) {
-      return true;
-    }
-    return false;
-  });
-  console.log(cart);
+    .findOne({ userId: req.user._id });
+  const existingProduct = cart.products.find(({ product }) => product == req.body.product);
+
   if (existingProduct) {
     updatedProducts = cart.products.map((item) => {
-      if (product == item.product._id) {
-        // eslint-disable-next-line no-return-assign
-        return { product: item.product._id, quantity: item.quantity + quantity };
+      if (req.body.product == item.product) {
+        return { product: item.product, quantity: item.quantity + req.body.quantity };
       }
       return item;
     });
   } else {
-    updatedProducts.push({ product, quantity });
+    updatedProducts = cart.products;
+    updatedProducts.push({ product: req.body.product, quantity: req.body.quantity });
   }
-  console.log(updatedProducts);
-  return res.status(200).end();
+
+  const updatedCart = await Cart
+    .findOneAndUpdate({ userId: req.user._id }, { products: updatedProducts }, { new: true })
+    .populate('products.product');
+  return res.status(200).json({ data: updatedCart });
 });
 
 exports.deleteCart = wrapAsync(async (req, res) => {
