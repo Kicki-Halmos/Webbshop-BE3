4d0788b1ae9a922a97c063797f0995edfbb0926b
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-10-22 10:18:59 +0200

    :sparkles: list all products on admin page

--- client/src/pages/ProductsAdmin.jsx
+++ client/src/pages/ProductsAdmin.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AdminNav from '../components/AdminNav';
+import ProductContext from '../contexts/product-context';
 
 export default function Admin() {
+  const productCtx = useContext(ProductContext);
+  const productList = productCtx.products;
+
+  useEffect(() => {
+    productCtx.getProducts();
+  }, []);
+
   return (
     <div>
       <AdminNav />
       <h1>List all products</h1>
       <Link to="/admin/create-product" style={{ textDecoration: 'none' }}>Create new product</Link>
+      {productList && (
+        productList.map((product) => (
+          <div className="col" key={product._id}>
+            <li>
+              {product.title}
+            </li>
+          </div>
+        ))
+      )}
     </div>
   );
 }
