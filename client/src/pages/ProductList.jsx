0e13d9a316c7dc519a6d3394dd2deed92208171c
Inna Dyachkova <inna.dyachkova@gmail.com>
2021-10-24 20:25:04 +0200

    :lipstick:Added more categories

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -67,6 +67,30 @@ const ProductList = () => {
       <div>
         <a id="Data & IT" className={genre === 'Data & IT' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Data & IT</a>
       </div>
+      <div>
+        <a id="Samhälle & politik" className={genre === 'Samhälle & politik' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Samhälle & politik</a>
+      </div>
+      <div>
+        <a id="Djur & natur" className={genre === 'Djur & natur' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Djur & natur</a>
+      </div>
+      <div>
+        <a id="Mat & dryck" className={genre === 'Mat & dryck' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Mat & dryck</a>
+      </div>
+      <div>
+        <a id="Ekonomi & ledarskap" className={genre === 'Ekonomi & ledarskap' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Ekonomi & ledarskap</a>
+      </div>
+      <div>
+        <a id="Biografier" className={genre === 'Biografier' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Biografier</a>
+      </div>
+      <div>
+        <a id="Resor" className={genre === 'Resor' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Resor</a>
+      </div>
+      <div>
+        <a id="Kultur" className={genre === 'Kultur' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Kultur</a>
+      </div>
+      <div>
+        <a id="Medicin" className={genre === 'Medicin' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Medicin</a>
+      </div>
       <div className="m-4 row">
         {renderProducts(products)}
       </div>
