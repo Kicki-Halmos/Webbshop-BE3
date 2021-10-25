4f06a03b6d237b59a8d118bb9163eae339f86a9d
Kicki Halmos <kickihalmos@gmail.com>
2021-10-25 20:50:21 +0200

    :sparkles:update adminRoutes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -9,12 +9,16 @@ const adminAuth = require('../middleware/adminAuth');
 
 const router = express.Router();
 // orders
-router.get('/', adminAuth, getAllOrders);
-router.get('/:id', adminAuth, getSingleOrder);
-router.put('/:id', adminAuth, uppdateOrder);
-router.delete('/:id', adminAuth, deleteOrder);
+router.get('/orders', adminAuth, getAllOrders);
+router.get('/orders/:id', adminAuth, getSingleOrder);
+router.put('/orders/:id', adminAuth, uppdateOrder);
+router.delete('/orders/:id', adminAuth, deleteOrder);
 
 // products
-router.get;
+router.get('/products', adminAuth, allProducts);
+router.get('/products/:id', adminAuth, findProduct);
+router.post('/products', adminAuth, addNewProduct);
+router.put('/products/:id', adminAuth, updateProduct);
+router.delete('/products/:id', adminAuth, deleteProduct);
 
 module.exports = router;
