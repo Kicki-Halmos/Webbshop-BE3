4e3bd026173504133b66bf36f1d61357a825b322
Inna Dyachkova <inna.dyachkova@gmail.com>
2021-10-26 10:44:37 +0200

    :art:dynamic rendering of categories

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -64,6 +64,9 @@ const ProductList = () => {
     products = filterByGenre();
   }
 
+  // const genres = [{name: 'Deckare', img: 'img/book.png'}, 'Skönlitteratur', 'Data & IT'];
+  const genres = ['Deckare', 'Skönlitteratur', 'Data & IT', 'Samhälle & politik', 'Djur & natur', 'Mat & dryck', 'Ekonomi & ledarskap', 'Biografier', 'Resor', 'Kultur', 'Medicin'];
+
   return (
     <div>
       <input
@@ -75,47 +78,13 @@ const ProductList = () => {
       />
       <div className="container">
         <div className="row row-cols-6">
-          <div className="col mb-4">
-            <img src="img/book.png" alt="Book" className="icon rounded d-block" />
-            <a id="Deckare" className={genre === 'Deckare' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Deckare</a>
-          </div>
-          <div className="col mb-4">
-            <img src="img/book.png" alt="Book" className="icon rounded d-block" />
-            <a id="Skönlitteratur" className={genre === 'Skönlitteratur' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Skönliteratur</a>
-          </div>
-          <div className="col mb-4">
-            <img src="img/book.png" alt="Book" className="icon rounded d-block" />
-            <a id="Data & IT" className={genre === 'Data & IT' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Data & IT</a>
-          </div>
-          <div className="col mb-4">
-            <img src="img/book.png" alt="Book" className="icon rounded d-block" />
-            <a id="Samhälle & politik" className={genre === 'Samhälle & politik' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Samhälle & politik</a>
-          </div>
-          <div className="col mb-4">
-            <img src="img/book.png" alt="Book" className="icon rounded d-block" />
-            <a id="Djur & natur" className={genre === 'Djur & natur' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Djur & natur</a>
-          </div>
-          <div className="col mb-4">
-            <img src="img/book.png" alt="Book" className="icon rounded d-block" />
-            <a id="Mat & dryck" className={genre === 'Mat & dryck' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Mat & dryck</a>
-          </div>
-        </div>
-        <div className="row row-cols-6">
-          <div className="col mb-4">
-            <a id="Ekonomi & ledarskap" className={genre === 'Ekonomi & ledarskap' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Ekonomi & ledarskap</a>
-          </div>
-          <div className="col mb-4">
-            <a id="Biografier" className={genre === 'Biografier' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Biografier</a>
-          </div>
-          <div className="col mb-4">
-            <a id="Resor" className={genre === 'Resor' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Resor</a>
-          </div>
-          <div className="col mb-4">
-            <a id="Kultur" className={genre === 'Kultur' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Kultur</a>
-          </div>
-          <div className="col mb-4">
-            <a id="Medicin" className={genre === 'Medicin' ? 'active' : ''} href="/" onClick={(event) => handleGenreChange(event)}>Medicin</a>
-          </div>
+          {genres.map((item) => (
+            <div className="col mb-4">
+              <img src="img/book.png" alt="Book" className="icon rounded d-block" />
+              <a id={item} className={`${genre === item ? 'active' : ''}`} href="/" onClick={(event) => handleGenreChange(event)}>{item}</a>
+            </div>
+
+          ))}
         </div>
       </div>
       <div className="m-4 row">
